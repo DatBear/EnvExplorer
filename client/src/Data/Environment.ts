a55db211ae840c428ee4e75650955cb52c0e0b07
Joe Rankin <jrankin312@gmail.com>
2023-02-12 17:18:46 -0600

    Fixed default allowed prefixes not working.

--- client/src/Data/Environment.ts
+++ client/src/Data/Environment.ts
@@ -14,7 +14,7 @@ class Environment {
   static getUniqueList(str: string | undefined, defaultValue: string[]) {
     var split = str?.split(',') ?? [];
     split = [...new Set([...split])];
-    return split.length === 0 ? defaultValue : split;
+    return split.length <= 1 ? defaultValue : split;
   }
 
   public static __initialize() {
@@ -34,26 +34,26 @@ class Environment {
   }
 
   public static templateValuesFromPrefix = (prefix: string) => {
-    const templateValues : Record<string, string> = {};
-    const templateOptions = this.defaultTemplate.split('/').map((x, idx) => ({pos: idx, value: x }))
+    const templateValues: Record<string, string> = {};
+    const templateOptions = this.defaultTemplate.split('/').map((x, idx) => ({ pos: idx, value: x }))
       .filter(x => x.value.indexOf('{') > -1 && x.value.indexOf('}') > -1)
-      .map(x => ({...x, value: x.value.replaceAll('{', '').replaceAll('}', '')}));
+      .map(x => ({ ...x, value: x.value.replaceAll('{', '').replaceAll('}', '') }));
     templateOptions.forEach(x => templateValues[x.value] = prefix.split('/')[x.pos]);
     return templateValues;
   };
 
   public static getSelectedTemplatePrefix = (selectedOptions: Record<string, string>) => {
     const opts = this.templateOptions();
-    let template = this.defaultTemplate.endsWith('/*') ? this.defaultTemplate.substring(0, this.defaultTemplate.length-2) : this.defaultTemplate;
+    let template = this.defaultTemplate.endsWith('/*') ? this.defaultTemplate.substring(0, this.defaultTemplate.length - 2) : this.defaultTemplate;
     opts.forEach(x => template = selectedOptions[x] && template.replaceAll(`{${x}}`, selectedOptions[x]));
     return template;
   }
 
   public static removeTemplate = (name: string) => {
-    const template = this.defaultTemplate.endsWith('/*') ? this.defaultTemplate.substring(0, this.defaultTemplate.length-2) : this.defaultTemplate;
+    const template = this.defaultTemplate.endsWith('/*') ? this.defaultTemplate.substring(0, this.defaultTemplate.length - 2) : this.defaultTemplate;
     return name.split('/').filter((_, idx) => idx >= template.split('/').length).join('/');
   }
-  
+
   public static getEnvFileParameter = (name: string, value: string) => {
     value = value.indexOf(' ') > 0 ? `"${value}"` : value;
     return `${this.removeTemplate(name).replaceAll('/', '__')}=${value}`;
